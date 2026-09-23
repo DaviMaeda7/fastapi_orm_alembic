538c48b894396ee53d8c08ba7b47683368f16b94
Davi Maeda <vidadavi777@gmail.com>
2026-09-22 21:04:15 -0300

    ajuste no main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 
 from fastapi import FastAPI
-from app.routes import alunos, disciplinas, notas, enderecos
+from app.routes import alunos, disciplinas, notas, endereco
 
 
 app = FastAPI()
 
 app.include_router(alunos.router)
-app.include_router(enderecos.router)
+app.include_router(endereco.router)
 app.include_router(disciplinas.router)
 app.include_router(notas.router)
